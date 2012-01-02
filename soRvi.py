060bd24495467edd7df4cbf57af8ab65a82f4bd9
Joona Lehtomaki <joona.lehtomaki@gmail.com>
2012-01-02 14:31:26 +0200

    Initial commit

---
+++ soRvi.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+This file is part of soRvi-manageR
+
+Copyright (C) 2011-12 Joona Lehtomäki
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public Licence as published by the Free Software
+Foundation; either version 2 of the Licence, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public Licence for more 
+details.
+
+You should have received a copy of the GNU General Public Licence along with
+this program (see LICENSE file in install directory); if not, write to the Free 
+Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
+This module is extension to manageR-plugin for QGIS By Carson J. Q. Farmer
+'''
+
+from PyQt4.QtGui import (QMessageBox)
+
+try:
+    import rpy2.robjects as robjects
+#  import rpy2.rinterface as rinterface
+except ImportError:
+    QMessageBox.warning(None , "manageR", "Unable to load manageR: Unable to load required package rpy2."
+                        + "\nPlease ensure that both R, and the corresponding version of Rpy2 are correctly installed.")
+
+from manageR import isLibraryLoaded
+ 
+isLibraryLoaded("sorvi")
+rdata = robjects.r["data"]
+data = rdata(package="sorvi")
+for key, value in data.iteritems():
+    if key == 'results':
+        rows_i = robjects.IntVector((1,4))
+        print value.rx(2, True)
